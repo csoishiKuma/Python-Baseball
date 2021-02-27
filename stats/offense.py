164be886bc938fe2bd1e60ee85773f309811c2e8
Craig Oishi <oisc@chevron.com>
2021-02-26 18:23:26 -0600

    Completed module

--- stats/offense.py
+++ stats/offense.py
@@ -1,2 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from data import games
+
+plays = games[games['type'] == 'play']
+plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
+plays = plays[plays.loc[:,'event'].str.contains('^(?:S(?!B)|D|T|HR)')]
+hits = plays[['inning', 'event']]
+hits.loc[:, ['inning']].apply(pd.to_numeric)
+print(hits)
+
+replacements = {
+    "r'^S(.*)'": 'single',
+    "r'^D(.*)'": 'double',
+    "r'^T(.*)'": 'triple',
+    "r'^HR(.*)'": 'hr'
+}
+hit_type = hits[hits['event']].replace(replacements, regex=True)
+print(hit_type)
